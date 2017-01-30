3f543f296e3220bd2814599169193782b53d0599
Kevin <kevchan85@gmail.com>
2017-01-30 11:18:40 -0800

    Added interface for special Move
    Interface for special move added
    
    Dire wolf now boosts cards through constructor.

--- src/Card.java
+++ src/Card.java
@@ -8,25 +8,44 @@
  *
  * @author rodney
  */
-public class Card {
+public abstract class Card {
     private int attackPoints, defensePoints, manaCost;
-    
+    SpecialMove specialMove;
+
     public Card(int attackPoints, int defensePoints, int manaCost){
         this.attackPoints = attackPoints;
         this.defensePoints = defensePoints;
         this.manaCost = manaCost;
     }
-    
+
     // Getters
     public int getAttackPoints(){
         return attackPoints;
     }
-    
+
     public int getDefensePoints(){
         return defensePoints;
     }
-    
+
     public int getManaCost(){
         return manaCost;
+    }
+
+    public void performSpecialMove(){
+        specialMove.move();
+    }
+
+    //setters
+    public void setAttackPoints(int attackPoints){
+        this.attackPoints =  attackPoints;
+    }
+    public void setDefensePoints(int defensePoints){
+        this.defensePoints = defensePoints;
+    }
+    public void setManaCost(int manaCost){
+        this.manaCost = manaCost;
+    }
+    public void setSpecialMove(SpecialMove move){
+        specialMove = move;
     }
 }
--- src/DireWolfAlpha.java
+++ src/DireWolfAlpha.java
@@ -4,4 +4,15 @@ public class DireWolfAlpha extends Card
     {
         super(2, 2, 2);
     }
+    public DireWolfAlpha(Card cardA, Card cardB)
+    {
+        super(2, 2, 2);
+        cardA.setAttackPoints(cardA.getAttackPoints() + 1);
+        cardB.setAttackPoints(cardB.getAttackPoints() + 1) ;
+    }
+    public DireWolfAlpha(Card cardA)
+    {
+        super(2, 2, 2);
+        cardA.setAttackPoints(cardA.getAttackPoints() + 1) ;
+    }
 }
