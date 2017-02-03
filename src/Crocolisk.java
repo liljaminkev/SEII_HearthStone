3cc7a9c31334b4d662969cc1549e679cf349472b
aprilaquino <bashi.aquino@gmail.com>
2017-02-03 06:20:43 -0800

    Added some minion and their test cases

---
+++ src/Crocolisk.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bashi.aquino
+ */
+public class Crocolisk extends Card{
+    
+    public Crocolisk()
+    {
+        super(2,3,2);
+    }
+}
