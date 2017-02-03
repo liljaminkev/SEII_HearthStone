4a5620d2de7f8b6e78fecc2b5976c2dc6f308eaa
Jmodar <jmodar@gmail.com>
2017-02-02 17:03:05 -0800

    Updated Player, Duel, and HearthDuel classes.

---
+++ src/BluegillWarrior.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author James
+ * 
+ */
+public class BluegillWarrior extends Card implements SpecialMove{
+    private int attackPoints, defensePoints, manaCost;
+    
+    /**
+     *
+     */
+    public BluegillWarrior() {
+        super(2, 1, 2);
+        
+    }
+    
+    //implement 
+    
+}
--- src/SpecialMove.java
+++ src/SpecialMove.java
@@ -3,4 +3,5 @@ public interface SpecialMove
     public void move();
     public void move(Card a, int specialAmmount);
     public void move(Card a, Card b, int specialAmmount);
+    public void move(Card a, Player p);
 }
