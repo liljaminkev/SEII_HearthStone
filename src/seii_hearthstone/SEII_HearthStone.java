32a09419ab678113b4d5d7b173b0c6132a04e9da
Kevin <kevchan85@yahoo.com>
2017-02-20 18:45:44 -0800

    test

--- src/seii_hearthstone/SEII_HearthStone.java
+++ src/seii_hearthstone/SEII_HearthStone.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cardgame;
+package seii_hearthstone;
 
 /**
  *
  * @author Kevin
  */
-public class CardGame {
+public class SEII_HearthStone {
 
     /**
      * @param args the command line arguments
