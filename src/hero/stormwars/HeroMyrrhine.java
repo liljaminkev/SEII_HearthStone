8de602fb2c53a720a590843b6846ccd8335d4981
rodney <rodney@134.154.48.246>
2017-02-22 20:05:43 -0800

    Added: CardPaltest CardSkirmisher HeroMyrrhine

---
+++ src/hero/stormwars/HeroMyrrhine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hero.stormwars;
+
+import hero.Hero;
+
+/**
+ *
+ * @author rodney
+ */
+public class HeroMyrrhine extends Hero{
+    public HeroMyrrhine(){
+        super("Myrrhine", 20);
+    }
+}
