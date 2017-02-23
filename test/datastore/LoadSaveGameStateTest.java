1c0532b27f8d5290bb3fa229539c7b49097f942e
Jmodar <jmodar@gmail.com>
2017-02-22 16:15:29 -0800

    Updated Load/Save test case
    Testing the load and save classes. Unfortunately, the project as it
    exists in the master branch still has many compile errors, so I'm unable
    to build.

--- test/datastore/LoadSaveGameStateTest.java
+++ test/datastore/LoadSaveGameStateTest.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package datastore;
+package test.datastore;
 
+import datastore.*;
 import playerassets.*;
 import hero.*;
 import hero.hearthstone.*;
 import hero.stormwars.*;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -23,8 +25,20 @@ import static org.junit.Assert.*;
 public class LoadSaveGameStateTest {
     Player p1, p2;
     Hero h1, h2;
+    Deck d1, d2;
+    Hand ha1, ha2;
+    ArrayList<Player> fromFile = null;
+
     
     public LoadSaveGameStateTest() {
+        SaveGameState.saveFile(p1, p2, "testSaveFile.txt");
+        fromFile = new AraryList<Player>();
+        fromFile = LoadGameState.loadGame("testSaveFile.txt");
+        
+        assertEquals("Jaina" == fromFile.get(0).getName());
+        assertEquals("Laertes" == fromFile.get(1).getName());
+        
+        
     }
     
     @Before
@@ -33,6 +47,11 @@ public class LoadSaveGameStateTest {
         p2 = new PlayerStormWars();
         h1 = new Jaina();
         h2 = new Laertes();
+        d1 = new Deck();
+        d2 = new Deck();
+        ha1 = new Hand();
+        ha2 = new Hand();
+        
     }
     
     
