0d1ff515a29cbc323389884e31ddf2840b745f27
Kevin <kevchan85@gmail.com>
2017-03-08 10:46:45 -0800

    Modified test and package names.

--- src/cards/hearthstone/abilities/BoostCrystals.java
+++ src/cards/hearthstone/abilities/BoostCrystals.java
@@ -1,7 +1,7 @@
 package cards.hearthstone.abilities;
-import playerassets.Player;
+import playerassets.PlayerHearthStone;
 import cards.Card;
-import cards.SpecialAbility;
+import playerassets.Player;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,9 +12,8 @@ import cards.SpecialAbility;
  *
  * @author James
  */
-public class BoostCrystals implements SpecialAbility{
+public class BoostCrystals implements HearthStoneAbilities{
 
-    @Override
     public void ability() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
@@ -29,8 +28,7 @@ public class BoostCrystals implements SpecialAbility{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public void ability(Card a, Player playerA) {
+    public void ability(Card a, PlayerHearthStone playerA) {
         playerA.incCrystals();
     }
 
@@ -41,6 +39,11 @@ public class BoostCrystals implements SpecialAbility{
 
     @Override
     public void setAbilityName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ability(Card a, Player playerA) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
